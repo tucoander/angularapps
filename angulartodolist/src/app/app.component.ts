132abc3e763c964d40e13a9aa745f91a47850796
Anderson Martins Farias <tuco.anderson@gmail.com>
2020-01-22 18:46:27 +0000

    fix app component

--- angulartodolist/src/app/app.component.ts
+++ angulartodolist/src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name:string = 'Anderson';
 
-  constructor(){
-    console.log('ready');
-    this.changeName('Olailton');
-  }
-
-  changeName(name:string):void {
-    this.name = name;
-  }
 }
