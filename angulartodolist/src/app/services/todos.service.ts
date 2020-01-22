ea590e4167197de6c573f1c8844ce2dc2dda80cd
Anderson Martins Farias <tuco.anderson@gmail.com>
2020-01-22 19:49:13 +0000

    servico de todo

--- angulartodolist/src/app/services/todos.service.ts
+++ angulartodolist/src/app/services/todos.service.ts
@@ -8,6 +8,22 @@ export class TodosService {
   constructor() { }
 
   getTodos(){
-    return[];
+    return[
+        {
+          id:1,
+          title: 'Todo One',
+          completed: false
+        },
+         {
+          id:2,
+          title: 'Todo Two',
+          completed: true
+        },
+         {
+          id:3,
+          title: 'Todo Three',
+          completed: false
+        },
+      ];
   }
 }
